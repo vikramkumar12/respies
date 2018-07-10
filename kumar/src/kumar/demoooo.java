53aeac2425c7eab89e2a3fd1ab2f8ee77a317496
A.Vivek kumar <A.Vivek kumar@vivek-pc>
2018-07-10 17:25:27 +0530

    updating

---
+++ kumar/src/kumar/demoooo.java
@@ -0,0 +1,15 @@
+package kumar;
+
+public class demoooo {
+
+	public static void main(String[] args) {
+		
+		
+		int x =10;
+		int y =20;
+		System.out.println(x+y);
+		// TODO Auto-generated method stub
+
+	}
+
+}
